a66fae497f6eb740b39c7c154f1779d7e981932b
ocirnexam <kontakt.maxmueller@outlook.com>
2025-06-14 10:10:26 +0200

    Added mcal, makefile, blinky test and build script

---
+++ build_and_flash.sh
@@ -0,0 +1,2 @@
+make
+sudo cp build/desi-c02-driver-test.bin /Volumes/NOD_F446RE/main.bin
---
+++ test/main.c
@@ -0,0 +1,32 @@
+#include "stm32f4xx.h"
+#include "gpio.h"
+#include "systemclock_config.h"
+#include "helpers.h"
+
+void SystemInit(void)
+{
+/* FPU settings ------------------------------------------------------------*/
+#if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
+    SCB->CPACR |= ((3UL << 10 * 2) | (3UL << 11 * 2)); /* set CP10 and CP11 Full Access */
+#endif
+}
+
+int main(void)
+{
+    if (SysClockConfig() == COUNTDOWN_EXCEEDED_ERROR)
+    {
+        return COUNTDOWN_EXCEEDED_ERROR;
+    }
+
+    // blinky code
+    IOPin led = {GPIOA, PIN5, GPIO_GENERAL_OUTPUT, GPIO_ALTF_SYSTEM};
+    GPIO_Config(led);
+    while (1)
+    {
+        GPIO_WritePin(led, PIN_HIGH);
+        msleep(500);
+        GPIO_WritePin(led, PIN_LOW);
+        msleep(500);
+    }
+    return 0;
+}
