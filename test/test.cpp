e62bc1580b729725be69fee4af42cf7841725a84
ocirnexam <maximilian.en.mueller@tuta.io>
2025-08-08 19:27:14 +0100

    Fixed build

--- test/test.cpp
+++ test/test.cpp
@@ -1,14 +1,6 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
-void SystemInit(void)
-{
-/* FPU settings ------------------------------------------------------------*/
-#if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
-    SCB->CPACR |= ((3UL << 10 * 2) | (3UL << 11 * 2)); /* set CP10 and CP11 Full Access */
-#endif
-}
-
 int main(void)
 {
     rcc_periph_clock_enable(RCC_GPIOA);
